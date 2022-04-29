9649a86b06b8e09190353960b9f84077c74bf958
sami081 <75616920+sami081@users.noreply.github.com>
2022-04-29 10:44:41 +0200

    version desktop header style

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const menu = document.getElementById("menu");
 const link = document.getElementById("link");
+const title = document.getElementById("title");
+const reservation = document.getElementById("reservation");
+ console.log(title);
 menu.addEventListener("click", () => {
-link.innerHTML= `
+  link.innerHTML = `
 <a href="./index.html">Accueil</a>
 <a href="./carte.html">Carte</a>
-`
-link.classList.toggle('link')
-})
+`;
+  link.classList.toggle("link");
+  title.classList.toggle("title2");
+  reservation.classList.toggle("reservation2")
+});
