72144ae056c3f9997b8ade36e7975c540e1bc769
sami081 <75616920+sami081@users.noreply.github.com>
2022-05-07 14:06:51 +0200

    modif font size

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const menu = document.getElementById("menu");
 const link = document.getElementById("link");
 const title = document.getElementById("title");
 const reservation = document.getElementById("reservation");
- console.log(title);
+const titleReservation = document.querySelector(".title-reservation")
+ 
 menu.addEventListener("click", () => {
   link.innerHTML = `
 <a href="./index.html">Accueil</a>
 <a href="./carte.html">Carte</a>
 `;
   link.classList.toggle("link");
-  title.classList.toggle("title2");
-  reservation.classList.toggle("reservation2")
+  // title.classList.toggle("title2");
+  // reservation.classList.toggle("reservation2")
+  titleReservation.classList.toggle("titleReservation2")
 });
