3636b323c6de4371963a6403cd46e5f4107eaa17
sami081 <75616920+sami081@users.noreply.github.com>
2022-06-13 14:25:25 +0200

    modif

--- contact.php
+++ contact.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Envoie d 'mail</title>
-</head>
-<body>
-  <?php
-  $retour = mail("samimakhloufi55@gmail.com", "envoi depuis la page contact", $_POST["lastName"], "")
-  ?>
-  
-</body>
-</html>
+<?php
+if (isset($_POST["lastName"])&&($_POST["firstName"])&&($_POST["tel"])&&($_POST["email"])&&($_POST["date"])&&($_POST["hour"])&&($_POST["number"])){
+  $retour = mail("samimakhloufi55@gmail.com", "Réservation", $_POST["lastName"], "Reply-to:" . $_POST["email"]);
+  if($retour) {
+    echo"<p> email envoyé
+  }
+}
